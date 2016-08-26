54635bbe02fe2c091270404a9999b5d3ee060283
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-08-26 16:47:14 -0700

    add delete to admin page for individual b&bs

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('bandb')
+  },
   actions: {
     bandbCreate(params) {
       var newBandb = this.store.createRecord('bandb', params);
       newBandb.save();
+    },
+    bandbDelete(bandb) {
+      var review_deletions = bandb.get('reviews').map(function(review) {
+        return review.destroyRecord();
+      });
+      Ember.RSVP.all(review_deletions).then(function() {
+        return bandb.destroyRecord();
+      });
     }
   }
 });
