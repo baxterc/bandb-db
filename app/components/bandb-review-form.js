e1e932cb5c5e166c3e89bb05e459b8be71f43d9c
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-08-26 12:49:31 -0700

    add working radio buttons for score using ember-radio-buttons

--- app/components/bandb-review-form.js
+++ app/components/bandb-review-form.js
@@ -2,15 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   showReviewForm: false,
+  score: 0,
   actions: {
     addReview(){
       this.set('showReviewForm', true);
+    },
+    scoreToggled(score){
+      console.log(score)
+      this.set('score', score);
     },
     reviewCreate() {
       var params = {
         author: this.get('review-author'),
         content: this.get ('review-content'),
-        score: this.get ('review-score'),
+        score: parseInt(this.get('score')),
         date: new Date(),
         bandb: this.get('bandb'),
       };
