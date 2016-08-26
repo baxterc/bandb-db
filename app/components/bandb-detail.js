7c5817c4542cca15f549142557ca0b114ec29b61
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-08-26 13:35:55 -0700

    add computed property for average score

--- app/components/bandb-detail.js
+++ app/components/bandb-detail.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  averageScore: Ember.computed('bandb.reviews.@each.score', function() {
+    var runningScore = 0;
+    this.get('bandb').get('reviews').forEach(function(review){
+      runningScore += parseInt(review.get('score'));
+    })
+    return runningScore / this.get('bandb').get('reviews').get('length');
+  }),
 });
